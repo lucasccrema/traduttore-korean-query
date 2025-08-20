9ffdc2dbba29f713e2d0492254e103ce1bdba049
lucasccrema <lucasccrema@gmail.com>
2025-08-20 10:40:20 +0200

    Update traduci.js

--- api/traduci.js
+++ api/traduci.js
@@ -28,8 +28,9 @@ export default async function handler(req, res) {
 });
     const data = await response.json();
     const traduzione = data.choices?.[0]?.message?.content || "Errore: nessuna traduzione trovata";
-
-    res.status(200).json({ traduzione });
+res.setHeader("Content-Type", "text/plain; charset=utf-8");
+    res.send(traduzione);
+   // res.status(200).json({ traduzione });
   } catch (error) {
     console.error(error);
     res.status(500).json({ errore: "Errore nella traduzione" });
